d958abb9670d468417e164da160f189fbb205e85
Peter <gudqo1995@naver.com>
2024-08-28 23:41:35 +0900

    login page

---
+++ app/04_group.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
+        <div className="group flex flex-col">
+          <input className="bg-gray-100 w-full" placeholder="write email" />
+          <span className="group-focus-within:block hidden">
+            please check email
+          </span>
+          <button>subbmit</button>
+        </div>
+      </div>
+    </main>
+  );
+}
