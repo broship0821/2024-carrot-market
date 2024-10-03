7f276506a377fdef817527191cd59cca5046ceca
Peter <gudqo1995@naver.com>
2024-10-03 21:03:47 +0900

    login, sms

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,9 +1,10 @@
 "use server";
+import {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+} from "@/lib/constants";
 import { z } from "zod";
-
-const passwordRegex = new RegExp(
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#?!@$%^&*-]).+$/
-);
 
 const checkUsername = (username: string) => !username.includes("potato");
 const checkPassword = ({
@@ -20,8 +21,6 @@ const formSchema = z
         invalid_type_error: "문자를 넣어야 합니다",
         required_error: "적으십시오",
       })
-      .min(3, "너무 짧읍니다")
-      .max(10, "너무 깁디다")
       .toLowerCase()
       .trim()
       .transform((username) => `${username} 바꿔버리기~`)
@@ -29,9 +28,9 @@ const formSchema = z
     email: z.string().email("올바른 이메일 형식이 아니읍니다").toLowerCase(),
     password: z
       .string()
-      .min(4)
-      .regex(passwordRegex, "소문자, 대문자, 숫자, 특수문자 포함 필요"),
-    confirm_password: z.string().min(4),
+      .min(PASSWORD_MIN_LENGTH)
+      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
     message: "비밀번호가 일치하지 않습니다",
