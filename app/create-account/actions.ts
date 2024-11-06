096ac056084506cf9d78d155303bf156c163c2cf
Peter <gudqo1995@naver.com>
2024-11-07 00:58:38 +0900

    database validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,6 +4,7 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const checkUsername = (username: string) => !username.includes("potato");
@@ -14,6 +15,33 @@ const checkPassword = ({
   password: string;
   confirm_password: string;
 }) => password === confirm_password;
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if (user) return false;
+  // else return true;
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -23,13 +51,16 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .transform((username) => `${username} 바꿔버리기~`)
-      .refine(checkUsername, "no potato"),
-    email: z.string().email("올바른 이메일 형식이 아니읍니다").toLowerCase(),
-    password: z
+      // .transform((username) => `${username} 바꿔버리기~`)
+      .refine(checkUsername, "no potato")
+      .refine(checkUniqueUsername, "이미 사용중인 이름입니다"),
+    email: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      .email("올바른 이메일 형식이 아니읍니다")
+      .toLowerCase()
+      .refine(checkUniqueEmail, "해당 이메일로 가입된 계정이 있읍니다"),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
@@ -44,7 +75,14 @@ export async function createAccount(prevState: any, formdata: FormData) {
     password: formdata.get("password"),
     confirm_password: formdata.get("confirm_password"),
   };
-  const result = formSchema.safeParse(data);
+  // const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
+  // refine 에 db 조회하는게 들어가면 safeParseAsync 사용 필요
   if (!result.success) return result.error.flatten();
-  else console.log(result.data);
+  else {
+    // hash password
+    // save the user to db
+    // log the user in
+    // redirect "/home"
+  }
 }
--- lib/db.ts
+++ lib/db.ts
@@ -2,17 +2,17 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "whatever",
-      user: {
-        connect: { id: 1 },
-      },
-    },
-  });
-  console.log(token);
-}
-test();
+// async function test() {
+//   const token = await db.sMSToken.create({
+//     data: {
+//       token: "whatever",
+//       user: {
+//         connect: { id: 1 },
+//       },
+//     },
+//   });
+//   console.log(token);
+// }
+// test();
 
 export default db;
