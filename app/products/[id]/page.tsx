a8f19353ef2d298a290c97291eeaaafcd6c0fd05
Peter <gudqo1995@naver.com>
2025-01-20 01:02:37 +0900

    check param

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,16 @@
+import { notFound } from "next/navigation";
+
 async function getProduct() {
   await new Promise((resolve) => setTimeout(resolve, 90000));
 }
 
 export default async function ProductDetail({
-  params: { id },
+  params,
 }: {
   params: { id: string };
 }) {
+  const id = Number(params.id);
+  if (isNaN(id)) return notFound();
   const product = await getProduct();
   return <span>Product detail of the profuct {id}</span>;
 }
